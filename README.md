20f5731c479fc16cea0a29f26dec140913eb2a35
matteobir12 <bir.matteo@gmail.com>
2024-05-04 15:20:17 -0700

    added run to readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # fps_from_scratch
  trying to make a simple fps from c++ and opengl
+
+mingw32-make all
+./build/main.exe
---
+++ src/ClientNetworking.cpp
@@ -0,0 +1,6 @@
+#include "ClientNetworking.h"
+#include "CommonStructs.h"
+
+void updateCharacterOnServer(Point3D p, Orientation o){
+    // to do
+}
